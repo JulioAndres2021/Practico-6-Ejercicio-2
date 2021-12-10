f6c097643f993ecf6d95f857d7569dd67314eca2
JulioAndres2021 <94810568+JulioAndres2021@users.noreply.github.com>
2021-12-10 07:58:44 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,11 @@
+function esparoimpar(){
+    var x    
+    x = document.getElementById("numeroparoimpar").value
+
+        if (x % 2 == 0) {
+         alert("El número es PAR");
+        }
+        else {
+            alert("El número es IMPAR");
+          }
+    }
